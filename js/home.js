371cd19fa57379b141ce51a398fd28aa62b36f93
Steven <steven.infinity29@gmail.com>
2017-04-14 22:54:59 -0400

    3rd version with fixed Stylings

--- js/home.js
+++ js/home.js
@@ -8,7 +8,7 @@
 },
 {
     "projectName": "Robotics",
-    "skills": ["allTechnicalSkills", "robotics", "allSoftSkills", "communication", "teamwork", "criticalThinking", "leadership"],
+    "skills": ["chosen", "allTechnicalSkills", "robotics", "allSoftSkills", "communication", "teamwork", "criticalThinking", "leadership"],
     "shortDescription": "(Jan-Oct 2015) Learn programming and constructing LEGO robots. Joined Local to International FLL Competitions",
     "longDescription": "We took part in FLL World Competition in Australia 2015 representing Indonesia, 8th National ’Robotics Explorer’ Competition (1st place) & Indonesian Robotics Olympiad (2nd Place). Tons of fun was involved in solving challenges within the team",
     "imgSrc": "resource/img/300x200/Robotics.jpg"
@@ -22,7 +22,7 @@
 },
 {
     "projectName": "Basketball Team",
-    "skills": ["allHobbies", "allSoftSkills", "teamwork", "communication", "sports"],
+    "skills": ["chosen","allHobbies", "allSoftSkills", "teamwork", "communication", "sports"],
     "shortDescription": "(2013-2015) Position: Centre/Forward",
     "longDescription": "We won 2nd in the annual SWA-CUP inter-school tournament 2015. It taught me a life lesson as we learned to identify and cover each other's weaknesses, teamwork, and gaining my ability to grab quick reflexes in game situations. Most of all we had fun.",
     "imgSrc": "resource/img/300x200/Basketball.jpg"
@@ -120,7 +120,7 @@
 },
 {
     "projectName": "4-months work Coop at Intellisoft Development Inc",
-    "skills": ["htmlcss", "net", "allTechnicalSkills", "communication", "teamwork", "allSoftSkills", "independence", "allSoftSkills"],
+    "skills": ["chosen", "htmlcss", "net", "allTechnicalSkills", "communication", "teamwork", "allSoftSkills", "independence", "allSoftSkills"],
     "shortDescription": "(2017 Jan-Apr) Jr. Web Developer",
     "longDescription": "First work experience and learned a lot of cool web skills. Gained experience in a real work environment guided by a team of experienced developers.",
     "imgSrc": "" 
@@ -134,7 +134,7 @@
 },
 {
     "projectName": "Earth Club Website",
-    "skills": ["htmlcss", "allTechnicalSkills", "communication", "allSoftSkills", "independence", "creativityplanning"],
+    "skills": ["chosen", "htmlcss", "allTechnicalSkills", "communication", "allSoftSkills", "independence", "creativityplanning"],
     "shortDescription": "(2015-2016) Built my first fully functional website for Earth Club as the client",
     "longDescription": "Was part of an IT project to find client, and completing the individual project following th SDLC life cycle",
     "imgSrc": ""
--- js/project.js
+++ js/project.js
@@ -1,28 +1,58 @@
 ﻿// external js: masonry.pkgd.js
 
-var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 250
-});
-$(".accordionPanel").on('click', function () {
-    if (!$(this).siblings('.accordion')[0].classList.contains('collapse')) {
-        return;   
-    }
-   
-    $this = $(this);
-    setTimeout(function () {
-        $this.parent().toggleClass("gridActive");
-        // trigger layout after item size changes
-        $grid.masonry('layout');
-
-        var $arrow = $this.find("i")[0];
-        if ($arrow.innerHTML == "keyboard_arrow_down") {
-            $arrow.innerHTML = "keyboard_arrow_up";
-        } else {
-            $arrow.innerHTML = "keyboard_arrow_down";
+function refreshMansory(){
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 360
+    });
+    $(".accordionPanel").on('click', function () {
+        if (!$(this).siblings('.accordion')[0].classList.contains('collapse')) {
+            return;
         }
 
-    }, 200);
-    
-    
-});
+        $this = $(this);
+        setTimeout(function () {
+            $this.parent().toggleClass("gridActive");
+            // trigger layout after item size changes
+            $grid.masonry('layout');
+
+            var $arrow = $this.find("i")[0];
+            if ($arrow.innerHTML == "keyboard_arrow_down") {
+                $arrow.innerHTML = "keyboard_arrow_up";
+            } else {
+                $arrow.innerHTML = "keyboard_arrow_down";
+            }
+
+        }, 200);
+
+    });
+}
+
+displayMansoryProjects();
+function displayMansoryProjects() {
+    var projectCount = 0;
+    for (var j = 0; j < projectJson.length; j++) {
+        var projectObj = projectJson[j];
+        if (projectObj["skills"].includes("chosen")) {
+            projectCount++;
+
+            var projectName = projectObj["projectName"];
+            var projectCaption = projectObj["shortDescription"];
+            var projectDesc = projectObj["longDescription"];
+            var projectImg = projectObj["imgSrc"];
+            var skills = projectObj["skills"].toString().replace(",",", ");
+
+            var htmlString = '<div class="grid-item grid-item--height4">';
+            htmlString+= '<div class="accordionPanel" href="#project'+projectCount+'" data-toggle="collapse">'
+            htmlString+= '<div class="innerPanel">&nbsp;<div class="projectImage"><img src="'+ projectImg +'"></div>'
+            htmlString+= '<div class="projectCaption"><b>'+projectName + '</b> - ' +projectCaption+'</div></div>';
+            htmlString+= '<div class="iconWrap"><i class="material-icons">keyboard_arrow_down</i></div></div>'
+            htmlString+= '<div id="project'+projectCount+'" class="accordion collapse">';
+            htmlString+= '<div class="hiddenAccordion">'+projectDesc+'</div></div></div>';
+
+            $(".grid").append(htmlString);
+            
+        }
+    }
+    refreshMansory();
+}
